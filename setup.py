eddcf436d1d3c92be7d367ff105034e8ed25bcd3
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2019-11-04 14:08:07 -0300

    Update setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+requires = [
+        'django'
+        ]
+
+setup(name='mysite',
+      version='1.0',
+      packages=find_packages(),
+      install_requires=requires,
+      scripts=['./projeto/manage.py'])
+
+
