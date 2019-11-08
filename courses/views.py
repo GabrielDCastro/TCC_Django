ebe33f1c057a0bcf9053e571b2d0f71a4cb3ff70
gabriel de castro <gabriel@localhost.localdomain>
2019-11-08 15:56:34 -0300

    view e listagem dos cursos

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Course
 
-# Create your views here.
+def cursos(request):
+    courses = Course.objects.all()
+    template_name = 'courses/cursos.html'
+    context = {
+        'courses': courses
+    }
+    return render (request, template_name, context)
