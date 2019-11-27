4f7268866f41b0efe18d972911cb808a89df5674
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2019-11-27 15:45:13 -0300

    Desfazendo as merdas do japones

--- home/url.py
+++ home/url.py
@@ -7,9 +7,5 @@ urlpatterns = [
     path('perfil/', views.perfil, name='perfil'),
     path('login/', views.login, name='login'),
     path('registrar/', views.registrar, name='registrar'),
-<<<<<<< HEAD
     path('duvidas/', views.duvidas, name='duvidas'),
-=======
-    #sdasddsadsadas
->>>>>>> 6dd834aa491fa527e51a7e60f713b0a2107a108e
 ]
