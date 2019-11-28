0cc557b65a29ceab61de15a352a99540d80cc558
GabrielDCastro <gabriel@localhost.localdomain>
2019-11-28 15:40:12 -0300

    processor de login

--- accounts/url.py
+++ accounts/url.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from django.conf.urls import include, url
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('home.url')),
+    path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
     path('account/', include('django.contrib.auth.urls')),
+]
